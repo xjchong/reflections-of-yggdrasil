5ce11cc38f6bfc4f06d05ee16bd13760193762ad
Xavier <xavier.chong.j@gmail.com>
2020-07-14 15:23:48 -0400

    Update tile styles (ascii and color)

--- src/main/kotlin/constants/GameColor.kt
+++ src/main/kotlin/constants/GameColor.kt
@@ -4,8 +4,10 @@ object GameColor {
 
     val WHITE = TileColor.fromString("#FFFFFF")
     val BLACK = TileColor.fromString("#000000")
-    val WALL_FOREGROUND = TileColor.fromString("#5a5032")
-    val WALL_BACKGROUND = TileColor.fromString("#5a5032")
-    val FLOOR_FOREGROUND = TileColor.fromString("#e6c896")
-    val FLOOR_BACKGROUND = TileColor.fromString("#e6c896")
+    val WALL_FOREGROUND = TileColor.fromString("#75715E")
+    val WALL_BACKGROUND = TileColor.fromString("#3E3D32")
+    val FLOOR_FOREGROUND = TileColor.fromString("#75715E")
+    val FLOOR_BACKGROUND = TileColor.fromString("#1E2320")
+    val DOOR_FOREGROUND = TileColor.fromString("#3E3D32")
+    val DOOR_BACKGROUND = TileColor.fromString("#2b1e0d")
 }
--- src/main/kotlin/model/GameTileRepository.kt
+++ src/main/kotlin/model/GameTileRepository.kt
@@ -3,26 +3,27 @@ package model
 import GameColor
 import org.hexworks.zircon.api.data.CharacterTile
 import org.hexworks.zircon.api.data.Tile
+import org.hexworks.zircon.api.graphics.Symbols
 
 object GameTileRepository {
 
     val EMPTY: CharacterTile = Tile.empty()
 
     val FLOOR: CharacterTile = Tile.newBuilder()
-            .withCharacter(' ')
+            .withCharacter(Symbols.INTERPUNCT)
             .withForegroundColor(GameColor.FLOOR_FOREGROUND)
             .withBackgroundColor(GameColor.FLOOR_BACKGROUND)
             .buildCharacterTile()
 
     val WALL: CharacterTile = Tile.newBuilder()
-            .withCharacter(' ')
+            .withCharacter('#')
             .withForegroundColor(GameColor.WALL_FOREGROUND)
             .withBackgroundColor(GameColor.WALL_BACKGROUND)
             .buildCharacterTile()
 
     val DOOR: CharacterTile = Tile.newBuilder()
             .withCharacter('+')
-            .withForegroundColor(GameColor.WALL_FOREGROUND)
-            .withBackgroundColor(GameColor.FLOOR_BACKGROUND)
+            .withForegroundColor(GameColor.DOOR_FOREGROUND)
+            .withBackgroundColor(GameColor.DOOR_BACKGROUND)
             .buildCharacterTile()
 }
