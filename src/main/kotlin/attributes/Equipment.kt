9d499658b80ac639322ab9bb30176a5827c66491
Xavier <xavier.chong.j@gmail.com>
2020-07-19 22:38:05 -0400

    Fix equipment in sidebar not updating

--- src/main/kotlin/attributes/Equipment.kt
+++ src/main/kotlin/attributes/Equipment.kt
@@ -6,7 +6,6 @@ import org.hexworks.cobalt.databinding.api.extension.createPropertyFrom
 import org.hexworks.cobalt.databinding.api.property.Property
 import org.hexworks.cobalt.datatypes.Maybe
 import org.hexworks.zircon.api.Components
-import org.hexworks.zircon.api.builder.component.TextBoxBuilder
 import org.hexworks.zircon.api.component.Component
 
 class Equipment(initialWeapon: Weapon? = null, initialArmor: Armor? = null): DisplayableAttribute {
@@ -34,17 +33,47 @@ class Equipment(initialWeapon: Weapon? = null, initialArmor: Armor? = null): Dis
         }()
 
     override fun toComponent(width: Int): Component {
-        val infoTextBox = Components.textBox(width)
+        val weaponNameLabel = Components.label()
+                .withText(weapon.optional?.name?.capitalize() ?: "None")
+                .withSize(width - 2, 1)
+                .build()
+        val weaponStatsLabel = Components.label()
+                .withText(weapon.optional?.statsString ?: "")
+                .withSize(width - 1, 1)
+                .build()
 
-        weapon.ifPresent { weapon ->
-            addCombatItemInfo(weapon, weaponProperty as Property<Maybe<CombatItem>>, infoTextBox, width)
+        val armorNameLabel = Components.label()
+                .withText(armor.optional?.name?.capitalize() ?: "None")
+                .withSize(width - 2, 1)
+                .build()
+        val armorStatsLabel = Components.label()
+                .withText(armor.optional?.statsString ?: "")
+                .withSize(width - 1, 1)
+                .build()
+
+        weaponProperty.onChange {
+            weaponNameLabel.textProperty.value = weapon.optional?.name?.capitalize() ?: "None"
+            weaponStatsLabel.textProperty.value = weapon.optional?.statsString ?: ""
         }
 
-        armor.ifPresent { armor ->
-            addCombatItemInfo(armor, armorProperty as Property<Maybe<CombatItem>>, infoTextBox, width)
+        armorProperty.onChange {
+            armorNameLabel.textProperty.value = armor.optional?.name?.capitalize() ?: "None"
+            armorStatsLabel.textProperty.value = armor.optional?.statsString ?: ""
         }
 
-        return infoTextBox.build()
+        return Components.textBox(width)
+                .addHeader("Weapon", withNewLine = false)
+                .addInlineComponent(weaponNameLabel)
+                .commitInlineElements()
+                .addInlineComponent(weaponStatsLabel)
+                .commitInlineElements()
+                .addNewLine()
+                .addHeader("Armour", withNewLine = false)
+                .addInlineComponent(armorNameLabel)
+                .commitInlineElements()
+                .addInlineComponent(armorStatsLabel)
+                .commitInlineElements()
+                .build()
     }
 
     fun equip(combatItem: CombatItem): CombatItem? {
@@ -67,29 +96,6 @@ class Equipment(initialWeapon: Weapon? = null, initialArmor: Armor? = null): Dis
         throw IllegalStateException("Equipment does not accept combat item of type ${combatItem.type::class}.")
     }
 
-    private fun addCombatItemInfo(item: CombatItem, itemProperty: Property<Maybe<CombatItem>>, infoTextBox: TextBoxBuilder, width: Int) {
-        val itemIcon = Components.icon().withIcon(item.iconTile).build()
-        val itemNameLabel = Components.label().withText(item.name).build()
-        val itemStatsLabel = Components.label()
-                .withText(item.statsString)
-                .withSize(width - 1, 1)
-                .build()
-
-        itemProperty.onChange {
-            itemIcon.iconProperty.value = item.iconTile
-            itemNameLabel.textProperty.value = item.name
-            itemStatsLabel.textProperty.value = item.statsString
-        }
-
-        infoTextBox.addHeader(item.combatType, withNewLine = false)
-                .addInlineComponent(itemIcon)
-                .addInlineComponent(itemNameLabel)
-                .commitInlineElements()
-                .addInlineComponent(itemStatsLabel)
-                .commitInlineElements()
-                .addNewLine()
-    }
-
     private val CombatItem.statsString: String
-        get() = "A : ${this.attackRating} D: ${this.defenseRating}"
+        get() = "A: ${this.attackRating} D: ${this.defenseRating}"
 }
