6144b0a8586988249d5d8694e6854a5eae52abb7
Xavier <xavier.chong.j@gmail.com>
2020-07-25 18:14:36 -0400

    Add health bar to target bar

--- src/main/kotlin/attributes/CombatStats.kt
+++ src/main/kotlin/attributes/CombatStats.kt
@@ -22,7 +22,7 @@ class CombatStats(
         private val maxHealthProperty: Property<Int>,
         val healthProperty: Property<Int> = createPropertyFrom(maxHealthProperty.value),
         private val maxStaminaProperty: Property<Int>,
-        private val staminaProperty: Property<Int> = createPropertyFrom(maxStaminaProperty.value),
+        val staminaProperty: Property<Int> = createPropertyFrom(maxStaminaProperty.value),
         private val powerProperty: Property<Double>,
         private val skillProperty: Property<Double>,
         private val luckProperty: Property<Double>
@@ -55,6 +55,89 @@ class CombatStats(
                         skillProperty = createPropertyFrom(skill),
                         luckProperty = createPropertyFrom(luck)
                 )
+
+        fun getBarComponent(textBoxBuilder: TextBoxBuilder, width: Int,
+                            valueProp: Property<Int>, maxValueProp: Property<Int>,
+                            color: TileColor): Component {
+            val barLabel = setupBar(textBoxBuilder, width, valueProp, maxValueProp, color)
+            valueProp.onChange { updateBar(barLabel, valueProp.value, maxValueProp.value) }
+
+            return barLabel
+        }
+
+        private fun setupBar(textBoxBuilder: TextBoxBuilder,
+                             width: Int,
+                             valueProp: Property<Int>,
+                             maxValueProp: Property<Int>,
+                             color: TileColor): Label {
+            val leftCapLabel = Components.label()
+                    .withSize(1, 1)
+                    .withText("${Symbols.SINGLE_LINE_T_DOUBLE_RIGHT}")
+                    .build()
+
+            val rightCapLabel = Components.label()
+                    .withSize(1, 1)
+                    .withText("${Symbols.SINGLE_LINE_T_DOUBLE_LEFT}")
+                    .build()
+
+            val barLabelSize = Size.create(width - 3, 1)
+            val barLabel = Components.label()
+                    .withSize(barLabelSize)
+                    .withText(getBarString(barLabelSize.width, valueProp.value, maxValueProp.value))
+                    .withStyle(color)
+                    .build()
+
+            barLabel.handleMouseEvents(MouseEventType.MOUSE_ENTERED) { _, _ ->
+                barLabel.textProperty.value = " ${valueProp.value}/${maxValueProp.value}"
+                Processed
+            }
+
+            barLabel.handleMouseEvents(MouseEventType.MOUSE_EXITED) { _, _ ->
+                updateBar(barLabel, valueProp.value, maxValueProp.value)
+                Processed
+            }
+
+            textBoxBuilder
+                    .addInlineComponent(leftCapLabel)
+                    .addInlineComponent(barLabel)
+                    .addInlineComponent(rightCapLabel)
+
+            return barLabel
+        }
+
+        private fun updateBar(barLabel: Label, value: Int, maxValue: Int) {
+            barLabel.textProperty.value = getBarString(barLabel.size.width, value, maxValue)
+        }
+
+        private fun getBarString(width: Int, value: Int, maxValue: Int): String {
+            val increment = (maxValue.toDouble() / width.toDouble()) / 4.0
+            var currentValue = value.toDouble()
+            var barString = ""
+
+            if (increment <= 0) {
+                return barString // Just a sanity check that increment != 0, which could loop.
+            }
+
+            while (currentValue > 0) {
+                if (currentValue - (increment * 4) > -increment) {
+                    barString += "${Symbols.BLOCK_SOLID}"
+                    currentValue -= (increment * 4)
+                } else if (currentValue - (increment * 3) > -increment) {
+                    barString += "${Symbols.BLOCK_DENSE}"
+                    currentValue -= (increment * 3)
+                } else if (currentValue - (increment * 2) > -increment) {
+                    barString += "${Symbols.BLOCK_MIDDLE}"
+                    currentValue -= (increment * 2)
+                } else if (currentValue - increment > -increment) {
+                    barString += "${Symbols.BLOCK_SPARSE}"
+                    currentValue -= increment
+                } else {
+                    break
+                }
+            }
+
+            return barString
+        }
     }
 
     override fun toComponent(width: Int): Component = Components.vbox()
@@ -62,8 +145,13 @@ class CombatStats(
             .build().apply {
                 val textBoxBuilder = Components.textBox(width)
 
-                val healthBarLabel = getHealthBarComponent(textBoxBuilder)
-                val staminaBarLabel = getStaminaBarComponent(textBoxBuilder)
+                val healthBarLabel = getBarComponent(textBoxBuilder, textBoxBuilder.size.width,
+                        healthProperty, maxHealthProperty, GameColor.DARK_GREEN)
+                textBoxBuilder.commitInlineElements()
+
+                val staminaBarLabel = getBarComponent(textBoxBuilder, textBoxBuilder.size.width,
+                        staminaProperty, maxStaminaProperty, GameColor.LIGHT_YELLOW)
+                textBoxBuilder.commitInlineElements()
 
                 val statsLabel = Components.label()
                         .withDecorations(box(boxType = BoxType.TOP_BOTTOM_DOUBLE))
@@ -84,59 +172,6 @@ class CombatStats(
                 addComponent(statsLabel)
             }
 
-    fun getHealthBarComponent(textBoxBuilder: TextBoxBuilder): Component {
-        val healthBarLabel = setupBar(textBoxBuilder, healthProperty, maxHealthProperty, GameColor.DARK_GREEN)
-        healthProperty.onChange { updateBar(healthBarLabel, health, maxHealth) }
-        return healthBarLabel
-    }
-
-    fun getStaminaBarComponent(textBoxBuilder: TextBoxBuilder): Component {
-        val staminaBarLabel = setupBar(textBoxBuilder, staminaProperty, maxStaminaProperty, GameColor.LIGHT_YELLOW)
-        staminaProperty.onChange { updateBar(staminaBarLabel, stamina, maxStamina) }
-        return staminaBarLabel
-    }
-
-    private fun setupBar(textBoxBuilder: TextBoxBuilder,
-                         valueProp: Property<Int>,
-                         maxValueProp: Property<Int>,
-                         color: TileColor): Label {
-        val leftCapLabel = Components.label()
-                .withSize(1, 1)
-                .withText("${Symbols.SINGLE_LINE_T_DOUBLE_RIGHT}")
-                .build()
-
-        val rightCapLabel = Components.label()
-                .withSize(1, 1)
-                .withText("${Symbols.SINGLE_LINE_T_DOUBLE_LEFT}")
-                .build()
-
-
-        val barLabelSize = Size.create(textBoxBuilder.size.width - 3, 1)
-        val barLabel = Components.label()
-                .withSize(barLabelSize)
-                .withText(getBarString(barLabelSize.width, valueProp.value, maxValueProp.value))
-                .withStyle(color)
-                .build()
-
-        barLabel.handleMouseEvents(MouseEventType.MOUSE_ENTERED) { _, _ ->
-            barLabel.textProperty.value = " ${valueProp.value}/${maxValueProp.value}"
-            Processed
-        }
-
-        barLabel.handleMouseEvents(MouseEventType.MOUSE_EXITED) { _, _ ->
-            updateBar(barLabel, valueProp.value, maxValueProp.value)
-            Processed
-        }
-
-        textBoxBuilder
-                .addInlineComponent(leftCapLabel)
-                .addInlineComponent(barLabel)
-                .addInlineComponent(rightCapLabel)
-                .commitInlineElements()
-
-        return barLabel
-    }
-
     fun regenStamina(amount: Int) {
         staminaProperty.value = (stamina + amount).coerceAtMost(maxStamina)
     }
@@ -147,35 +182,5 @@ class CombatStats(
 
     fun dockHealth(amount: Int) {
         healthProperty.value = (health - amount).coerceAtLeast(0)
-    }
-
-    private fun updateBar(barLabel: Label, value: Int, maxValue: Int) {
-        barLabel.textProperty.value = getBarString(barLabel.size.width, value, maxValue)
-    }
-
-    private fun getBarString(width: Int, value: Int, maxValue: Int): String {
-        val increment = (maxValue.toDouble() / width.toDouble()) / 4.0
-        var currentValue = value.toDouble()
-        var barString = ""
-
-        while (currentValue > 0) {
-            if (currentValue - (increment * 4) > -increment) {
-                barString += "${Symbols.BLOCK_SOLID}"
-                currentValue -= (increment * 4)
-            } else if (currentValue - (increment * 3) > -increment) {
-                barString += "${Symbols.BLOCK_DENSE}"
-                currentValue -= (increment * 3)
-            } else if (currentValue - (increment * 2) > -increment) {
-                barString += "${Symbols.BLOCK_MIDDLE}"
-                currentValue -= (increment * 2)
-            } else if (currentValue - increment > -increment) {
-                barString += "${Symbols.BLOCK_SPARSE}"
-                currentValue -= increment
-            } else {
-                break
-            }
-        }
-
-        return barString
     }
 }
--- src/main/kotlin/attributes/FocusTarget.kt
+++ src/main/kotlin/attributes/FocusTarget.kt
@@ -1,5 +1,6 @@
 package attributes
 
+import GameColor
 import entity.AnyEntity
 import entity.getAttribute
 import extensions.optional
@@ -12,17 +13,31 @@ import org.hexworks.zircon.api.component.Component
 class FocusTarget : DisplayableAttribute {
 
     var targetProperty: Property<Maybe<AnyEntity>> = createPropertyFrom(Maybe.empty())
+    private val healthProperty: Property<Int> = createPropertyFrom(0)
+    private val maxHealthProperty: Property<Int> = createPropertyFrom(1)
 
     override fun toComponent(width: Int): Component {
+        val textBoxBuilder = Components.textBox(width - 2)
         val nameLabel = Components.label()
                 .withText("")
                 .withSize(16, 1)
                 .build()
 
+        textBoxBuilder.addInlineComponent(nameLabel)
+
+        val healthBarLabel = CombatStats.getBarComponent(textBoxBuilder, 20,
+                healthProperty, maxHealthProperty, GameColor.DARK_GREEN)
+
         targetProperty.onChange {
+            val combatStatsCopy = it.newValue?.optional?.getAttribute(CombatStats::class)
+
             nameLabel.textProperty.value = it.newValue.optional?.name?.capitalize() ?: ""
+            healthProperty.updateValue(combatStatsCopy?.health ?: 0)
+            maxHealthProperty.updateValue(combatStatsCopy?.maxHealth ?: 1)
+
             it.newValue.ifPresent { target ->
                 target.getAttribute(CombatStats::class)?.healthProperty?.onChange {
+                    healthProperty.updateValue(it.newValue)
                     if (it.newValue <= 0) {
                         targetProperty.updateValue(Maybe.empty())
                     }
@@ -30,6 +45,6 @@ class FocusTarget : DisplayableAttribute {
             }
         }
 
-        return nameLabel
+        return textBoxBuilder.commitInlineElements().build()
     }
 }
