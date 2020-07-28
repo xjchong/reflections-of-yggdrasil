c32bd6db7dc1992e755e0acc787b184beb395764
Xavier <xavier.chong.j@gmail.com>
2020-07-28 10:49:12 -0400

    Update dropping to drop items in a spread rather than stack if possible

--- src/main/kotlin/facets/passive/Droppable.kt
+++ src/main/kotlin/facets/passive/Droppable.kt
@@ -3,6 +3,8 @@ package facets.passive
 import attributes.Inventory
 import commands.Drop
 import entity.getAttribute
+import extensions.neighbors
+import extensions.optional
 import game.GameContext
 import org.hexworks.amethyst.api.Command
 import org.hexworks.amethyst.api.Consumed
@@ -13,11 +15,28 @@ import org.hexworks.amethyst.api.entity.EntityType
 object Droppable : BaseFacet<GameContext>() {
 
     override suspend fun executeCommand(command: Command<out EntityType, GameContext>): Response {
-        return command.responseWhenCommandIs(Drop::class) { (context, droppable, owner, position) ->
+        return command.responseWhenCommandIs(Drop::class) { (context, droppable, owner, idealPosition) ->
             owner.getAttribute(Inventory::class)?.let { inventory -> inventory.remove(droppable) }
 
             with (context.world) {
-                addEntity(droppable, position)
+                var actualDropPosition = idealPosition
+
+                // Attempt to drop the entity somewhere unoccupied. Otherwise, just stack it at the original position.
+                fetchBlockAt(idealPosition).ifPresent { block ->
+                    if (!block.isUnoccupied) {
+                        for (neighborPos in idealPosition.neighbors()) {
+                            val neighborBlock = fetchBlockAt(neighborPos).optional
+
+                            if (neighborBlock != null && neighborBlock.isUnoccupied) {
+                                actualDropPosition = neighborPos
+                                break
+                            }
+                        }
+                    }
+                }
+
+
+                addEntity(droppable, actualDropPosition)
                 observeSceneBy(owner, "The $owner drops the $droppable.")
             }
 
